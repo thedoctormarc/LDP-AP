e21f77097315d63ae10f805fc51cd3f999d4e3fc
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2020-08-05 11:29:40 +0200

    Simple working test

---
+++ Automation Testing/Assets/Editor/AutomationAttempt_Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class AutomationAttempt_Test
+{
+    [Test]
+    public void Do_Test()
+    {
+        int a = 10;
+        Assert.That(a, Is.EqualTo(11));
+    }
+}
