11814da3127484e608308ef25bcba25fa6e71c2c
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-03-02 13:11:34 +0100

    Testing basic animator with a swat character combined with pathfinding

---
+++ Navigation Testing/NavigationTest/Assets/Scripts/SwatLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwatLogic : MonoBehaviour
+{
+    Pathfinding.AIPath path;
+    GameObject swat;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = GetComponent<Pathfinding.AIPath>();
+        swat = transform.GetChild(0).gameObject;
+        animator = swat.GetComponent<Animator>();
+
+        StartMoving();
+    }
+
+    void StartMoving()
+    {
+        path.canSearch = true;
+        path.canMove = true;
+        animator.SetBool("Moving", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
