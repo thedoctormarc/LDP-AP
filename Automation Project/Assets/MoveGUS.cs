abd13edbc09d8c15fe2cf3072ec55779a8cfdf9b
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-04-08 15:38:46 +0200

    Avoid moving obstacle

---
+++ Automation Project/Assets/MoveGUS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class MoveGUS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        transform.position += new Vector3(Time.deltaTime, 0, 0);
+        GetComponent<GraphUpdateScene>().Apply();
+    }
+}
