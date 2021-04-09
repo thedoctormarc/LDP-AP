45c069887665ad4c71fef4a6f4c2a64177c4857a
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-04-09 12:30:09 +0200

    Beginnig to implement actor behaviour with node canvas

---
+++ Automation Project/Assets/Scripts/T_Relocate.cs
@@ -0,0 +1,21 @@
+﻿using NodeCanvas.Framework;
+using UnityEngine;
+using Pathfinding;
+
+public class T_Relocate : ActionTask
+{
+    AIPath path;
+
+    protected override string OnInit()
+    {
+        path = agent.gameObject.GetComponent<AIPath>();
+        path.destination = new Vector3(14f, 0f, 99f);
+
+        return null;
+    }
+
+    protected override void OnExecute()
+    {
+
+    }
+}
